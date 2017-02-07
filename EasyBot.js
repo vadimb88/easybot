1d66ff2f2a68e2ab3e9e58a36b8eb65c69e94180
Vadim Babaev <fair210@gmail.com>
2017-02-07 19:00:40 +0300

    Added multiply event handlers. Added forward, back and refresh functions. Some functions changed their name.

--- EasyBot.js
+++ EasyBot.js
@@ -1,11 +1,50 @@
 'use strict';
+require('./polifills').polifills();
 var Promise = require('./libs/bluebird.min');
 var webpage = require('webpage');
+
 var fs = require('fs');
+
+var EVENTS = [''];
+
+var createEventHandler = function (ctx, eventName) {
+  return function () {
+    console.log('Running ' + eventName + ' handler');
+    var deleted = false;
+    var args = Array.prototype.slice.apply(arguments);
+    if (ctx.handlers[eventName] !== undefined) {    
+      ctx.handlers[eventName].forEach(function (handler, ind) { 
+        if (handler === null) return;
+        
+        handler.fn.apply(ctx, args);
+        if (handler.once) {
+          ctx.handlers[eventName][ind] = null;
+          deleted = true;          
+        }
+      });
+    }
+
+    if (deleted) {
+      ctx.handlers[eventName] = ctx.handlers[eventName].filter(function(el) { return el !== null});
+    }    
+  };
+};
 
 var EasyBot = function (options) {
   this.queue = Promise.resolve();
   this.page = webpage.create();
+  this.handlers = {'onLoadStarted': [ 
+    {
+      fn: function () { console.log('MY LOAD STARTED'); },
+      once: true
+    },
+    {
+      fn: function () { console.log('MY LOAD STARTED2'); },
+      once: false
+    }
+  ]};
+
+  this.page.onLoadStarted = createEventHandler(this, 'onLoadStarted');
 };
 
 EasyBot.prototype.addToQueue = function (cb) {
@@ -18,7 +57,7 @@ EasyBot.prototype.then = function (cb) {
   return this;
 };
 
-EasyBot.prototype.openUrl = function (url) {
+EasyBot.prototype.goto = function (url) {
   var self = this;
   this.queue = this.queue.then(function () {
     return new Promise(function (resolve, reject) { 
@@ -39,6 +78,116 @@ EasyBot.prototype.openUrl = function (url) {
   return this;
 };
 
+EasyBot.prototype.back = function () {
+  /* Page goBack function is async but it don't take any callbacks.
+   * So i wraped it in Promise and resolve this promise after onLoadFinished event.
+   */
+  
+  return this.addToQueue(function () {    
+    var self = this;
+    console.log('Going back ' + self.page.canGoBack);
+    if (!self.page.canGoBack) {
+      return false;
+    }
+
+    return new Promise(function (resolve, reject) {     
+        addEventListenerSync.call(self, 'onLoadFinished', function () { console.log('LOAD FINISHED WITH CALLBACK'); resolve(true); }, true);
+        self.page.goBack();          
+    });   
+  });
+};
+
+EasyBot.prototype.forward = function () {
+  /* Page goForward function is async but it don't take any callbacks.
+   * So i wraped it in Promise and resolve this promise after onLoadFinished event.
+   */
+
+  return this.addToQueue(function () {    
+    var self = this;
+    console.log('Going Forward ' + self.page.canGoForward);
+    if (!self.page.canGoForward) {
+      return false;
+    }
+
+    return new Promise(function (resolve, reject) {     
+        addEventListenerSync.call(self, 'onLoadFinished', function () { console.log('LOAD FINISHED WITH CALLBACK'); resolve(true); }, true);
+        self.page.goForward();          
+    });   
+  });
+};
+
+EasyBot.prototype.refresh = function () {
+  /* Page reload function is async but it don't take any callbacks.
+   * So i wraped it in Promise and resolve this promise after onLoadFinished event.
+   */
+
+  return this.addToQueue(function () {    
+    var self = this;
+    console.log('Reloadind page with url: ' + self.page.url);
+    return new Promise(function (resolve, reject) {     
+        addEventListenerSync.call(self, 'onLoadFinished', function () { console.log('LOAD FINISHED WITH CALLBACK'); resolve(true); }, true);
+        self.page.reload();          
+    });   
+  }); 
+};
+
+var addEventListenerSync = function (type, listener, once) {
+  once = once === undefined ? false : once;
+  console.log('Adding event Listener for: ' + type);
+  if(typeof listener !== 'function') {
+    return false;
+  }
+
+  if(typeof this.page[type] !== 'function') {
+    this.page[type] = createEventHandler(this, type);
+  }
+
+  if(this.handlers[type] === undefined) {
+    this.handlers[type] = [];
+  }
+
+  this.handlers[type].push({ fn: listener, once: once });
+  return true;  
+};
+
+var removeEventListenerSync = function (type, listener) {  
+    console.log('Removing event Listener for: ' + type);
+    if(this.handlers[type] === undefined) return true;
+
+    var listenerIndex = this.handlers[type].findIndex(function (el) { console.log('SS ' + el); return el !== null && el.fn === listener; });
+    console.log('Listener index: ' + listenerIndex);
+    if(listenerIndex !== -1) {
+      this.handlers[type].splice(listenerIndex, 1);
+    }
+
+    return true; 
+};
+
+EasyBot.prototype.addEventListener = function (type, listener, once) {
+  once = once === undefined ? false : once;
+  return this.addToQueue(function () {
+    return addEventListenerSync.call(this, type, listener, once);
+  });  
+};
+
+EasyBot.prototype.removeEventListener = function (type, listener) {
+  return this.addToQueue(function () {
+    return removeEventListenerSync.call(this, type, listener);
+  });  
+};
+
+EasyBot.prototype.click = function (selector) {
+  return this.evaluate(function (selector) {
+    someElement = document.querySelector(selector);
+    if (someElement !== null) {
+      makeClick(someElement);
+      return true;
+    }
+
+    return false;
+  }, selector);
+};
+
 EasyBot.prototype.evaluate = function () {
   var args = Array.prototype.slice.apply(arguments);
   console.log('Arguments ' + args);
@@ -50,7 +199,7 @@ EasyBot.prototype.evaluate = function () {
   return this;
 };
 
-EasyBot.prototype.fill = function (selector, text, options) {
+EasyBot.prototype.type = function (selector, text, options) {
   var clean = options !== undefined && options.clean !== undefined ? options.clean : true;
 
   return this.evaluate(function (selector, clean) {
